18f39237642724d74888d59889b97100184585b7
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-16 21:22:07 +0530

    Time: 38 ms (39.39%), Space: 47.1 MB (62.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // brute force Tc- o(N^2)
+        // SC = O(1)
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((nums[i]+nums[j])==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int []{-1,-1};
+    }
+}

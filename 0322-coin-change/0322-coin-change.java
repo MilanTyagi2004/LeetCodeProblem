97b31283fcc6dc1c08246879aaf28f9c1800f7b0
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-17 14:10:07 +0530

    Time: 40 ms (8.6%), Space: 49.2 MB (5.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,25 @@
 class Solution {
+    int memo(int i, int n,int target,int coins[],Integer [][]dp){
+        if(target==0){
+            return 0;
+        }
+        if(i==n){
+            return (int) 1e9;
+        }
+        if(dp[i][target]!=null){
+            return dp[i][target];
+        }
+        int take =(int) 1e9;
+        if(coins[i]<=target){
+        take = 1+ memo(i,n,target-coins[i],coins,dp);
+        }
+        int notTake = memo(i+1,n,target,coins,dp);
+        return dp[i][target] = Math.min(take,notTake);
+    }
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        for(int i=0;i<=amount;i++){
-            dp[i] = amount+1;
-        }
-        dp[0] = 0;
-
-        for (int i = 0; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
-        }
-
-        return dp[amount] > amount ? -1 : dp[amount];
+        Integer dp [] [] = new Integer[coins.length][amount+1];
+        int n = coins.length;
+        int ans = memo(0,n,amount,coins,dp);
+        return (ans>=(int) 1e9)?-1:ans;
     }
 }

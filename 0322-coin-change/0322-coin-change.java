46b353cb70775d88568ac8e80d4d023d5d27d3d8
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-17 14:20:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -17,9 +17,28 @@ class Solution {
         return dp[i][target] = Math.min(take,notTake);
     }
     public int coinChange(int[] coins, int amount) {
-        Integer dp [] [] = new Integer[coins.length][amount+1];
+        Integer dp [] [] = new Integer[coins.length+1][amount+1];
+
         int n = coins.length;
-        int ans = memo(0,n,amount,coins,dp);
-        return (ans>=(int) 1e9)?-1:ans;
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int t = 0;t<=amount;t++){
+            dp[n][t] = (int) 1e9;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j = 1;j<=amount;j++){
+                int take = (int) 1e9;
+                if(coins[i]<=j){
+                    take = 1+ dp[i][j-coins[i]];
+                }
+                int notTake = dp[i+1][j];
+                dp[i][j] = Math.min(take,notTake);
+            }
+        }
+        int ans =dp[0][amount];
+        return (ans>=(int)1e9) ? -1 : ans;
     }
 }

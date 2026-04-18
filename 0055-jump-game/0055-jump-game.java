a4fcbd7cb611f85728283ef69b7ee5f96e2795c9
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-18 13:06:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,15 @@
 class Solution {
-    boolean helper(int i,int[] nums,Boolean dp[]){
-        if(i==nums.length-1){
-            return true;
-        }
-        if(i>=nums.length){
-            return true;
-        }
-        if(dp[i]!=null){
-            return dp[i];
-        }
-        for(int idx =1;idx<=nums[i];idx++){
-            if(helper(i+idx,nums,dp)==true){
-                return dp[i] =true;
+    public boolean canJump(int[] nums) {
+        boolean dp[]  = new boolean [nums.length];
+        dp[nums.length-1] = true;
+        for(int i=nums.length-2;i>=0;i--){
+            for(int j=1;j<=nums[i] && i + j < nums.length;j++){
+                if(dp[i+j]){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return dp[i] =false;
-    }
-    public boolean canJump(int[] nums) {
-        Boolean dp[]= new Boolean[nums.length+1];
-        return helper(0,nums,dp);
+        return dp[0];
     }
 }

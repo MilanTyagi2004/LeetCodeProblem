8da1b474f4acb18f122b3c4ce6a4c8903f349f18
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-20 17:21:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j=0;
+        int k=0;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[j]){
-                j++;
-                nums[j] = nums[i];
+            if(nums[i]!=nums[k]){
+                k++;
+                nums[k] =nums[i];
             }
         }
-        return j+1;  
+        return k+1;
     }
 }

3a15da1f4ee9cbc869d1de11671fd6ddf7b16978
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-04-30 17:43:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0], b[0]));
-        // int [][] newArr = new int[][];
-        ArrayList<int []> al = new ArrayList<>();
-        int first = intervals[0][0];
-        int second = intervals[0][1];
-        for(int i=1;i<intervals.length;i++){
-            if(second >=intervals[i][0]){
-            second  = Math.max(second, intervals[i][1]);
+    public int[][] merge(int[][] arr) {
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int first = arr[0][0];
+        int second=  arr[0][1];
+        ArrayList<int []>al = new ArrayList<>();
+        for(int i=1;i<arr.length;i++){
+            if(second>=arr[i][0]){
+                second = Math.max(second,arr[i][1]);
             }else{
-                al.add(new int[]{first,second});
-                first = intervals[i][0];
-                second = intervals[i][1];
+            al.add(new int []{first,second});
+            first = arr[i][0];
+            second = arr[i][1];
             }
         }
-        al.add(new int[]{first,second});
-        return al.toArray(new int [al.size()][]);
+        al.add(new int []{first,second});
+        return al.toArray(new int[al.size()][]);
     }
 }

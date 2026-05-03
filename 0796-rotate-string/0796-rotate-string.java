0a190fc37ca31cdf08a058a82c865c18ad1b72a2
Milan Tyagi <157273416+MilanTyagi2004@users.noreply.github.com>
2026-05-03 06:52:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        char [] arr=s.toCharArray();
-        int n=  s.length();
-        for(int k=0;k<n;k++){
-            char temp = arr[0];
-            for(int i=1;i<n;i++){
-                arr[i-1] =arr[i];
-            }
-            arr[n-1] =temp;
-            String str = new String(arr);
-            if(str.equals(goal)){
-                return true;
-            }
+        if(s.length() != goal.length()){
+            return false;
         }
-        return false;
+
+        StringBuilder sb  =new StringBuilder(s);
+        sb.append(s);
+        String str = sb.toString();
+        return str.contains(goal);
     }
 }

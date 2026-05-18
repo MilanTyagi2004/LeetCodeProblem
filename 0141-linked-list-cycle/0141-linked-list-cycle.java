32038fd661906b9dc8d1d18af81f29ee65ddbc08
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-18 20:39:58 +0530

    Time: 0 ms (100%), Space: 47.1 MB (15.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,8 +12,7 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode s = head;
-        ListNode f = head;
-
+        ListNode f= head;
         while(f!=null && f.next!=null){
             s = s.next;
             f = f.next.next;

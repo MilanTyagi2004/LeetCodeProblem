54bcd8b326e21b2c8d3a6f48c9867d6d5dd4b071
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-20 21:11:10 +0530

    Time: 11 ms (96.28%), Space: 46.3 MB (70.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,44 +1,13 @@
 class Solution {
-    int memo(int i, int n,int target,int coins[],Integer [][]dp){
-        if(target==0){
-            return 0;
-        }
-        if(i==n){
-            return (int) 1e9;
-        }
-        if(dp[i][target]!=null){
-            return dp[i][target];
-        }
-        int take =(int) 1e9;
-        if(coins[i]<=target){
-        take = 1+ memo(i,n,target-coins[i],coins,dp);
-        }
-        int notTake = memo(i+1,n,target,coins,dp);
-        return dp[i][target] = Math.min(take,notTake);
-    }
-    public int coinChange(int[] coins, int amount) {
-        Integer dp [] [] = new Integer[coins.length+1][amount+1];
-
-        int n = coins.length;
-
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int t = 0;t<=amount;t++){
-            dp[n][t] = (int) 1e9;
-        }
-
-        for(int i=n-1;i>=0;i--){
-            for(int j = 1;j<=amount;j++){
-                int take = (int) 1e9;
-                if(coins[i]<=j){
-                    take = 1+ dp[i][j-coins[i]];
-                }
-                int notTake = dp[i+1][j];
-                dp[i][j] = Math.min(take,notTake);
+    public int coinChange(int[] nums, int amount) {
+        int [] dp =  new int[amount+1];
+        Arrays.fill(dp,(int)1e9);
+        dp[0] = 0;
+        for(int i:nums){
+            for(int j=i;j<=amount;j++){
+                dp[j] = Math.min(dp[j],1+dp[j-i]);
             }
         }
-        int ans =dp[0][amount];
-        return (ans>=(int)1e9) ? -1 : ans;
+        return dp[amount]==(int)1e9?-1:dp[amount];
     }
 }

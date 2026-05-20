bf6a1da3ea73086d79952c3811207bf052d4eaa5
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-20 09:33:47 +0530

    Time: 2 ms (94.83%), Space: 45.9 MB (32.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,18 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrace(candidates, target, 0, new ArrayList<>(), res);
-        return res;
+    void helper(int idx,int n,int nums[],List<List<Integer>>all,List<Integer>al,int target){
+        if(idx==n ){
+            if(target==0){
+            all.add(new ArrayList<>(al));
+        }
+            return;
+        }
+        if(target>=nums[idx]){
+            al.add(nums[idx]);
+            helper(idx,n,nums,all,al,target-nums[idx]);
+            al.remove(al.size()-1);
+        }
+        helper(idx+1,n,nums,all,al,target);
     }
-    public void backtrace(int [] candidates,int target, int start,List<Integer> path, List<List<Integer>> res){
-        if(target<0)return ;
-        if(target==0){
-            res.add(new ArrayList<>(path));
-        }
-        for(int i =start;i<candidates.length;i++){
-            path.add(candidates[i]);
-            backtrace(candidates, target - candidates[i], i, path, res); // not i+1 since we can reuse elements
-            path.remove(path.size() - 1); // backtrack
-        }
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>>all = new ArrayList<>();
+        helper(0,n,nums,all,new ArrayList<>(),target);
+        return all;
     }
 }

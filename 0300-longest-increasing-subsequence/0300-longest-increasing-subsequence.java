48cb3edf8a33b65c923386c9710219e1c65b296c
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-20 21:27:54 +0530

    Time: 414 ms (5.13%), Space: 273.2 MB (6.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,34 +1,18 @@
 class Solution {
-    int recursion(int i,int prev,int []nums,Integer dp[][]){
-        if(i==nums.length){
-            return 0;
+    int func(int idx,int prev,int nums[],Integer[][]dp){
+        if(idx==nums.length){return 0;}
+        if(dp[idx][prev+1]!=null){
+            return dp[idx][prev+1];
         }
-        if(dp[i][prev+1]!=null){
-            return dp[i][prev+1];
+        int t = 0;
+        if(prev==-1 || nums[prev]<nums[idx]){
+            t = 1+func(idx+1,idx,nums,dp);
         }
-        int nottake = recursion(i+1,prev,nums,dp);
-        int take = 0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            take = 1 + recursion(i+1,i,nums,dp);
-        }
-        return dp[i][prev+1] = Math.max(take,nottake);
+        int nt = func(idx+1,prev,nums,dp);
+        return dp[idx][prev+1] = Math.max(t,nt);
     }
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        Integer dp[] [] = new Integer [nums.length+1][nums.length+1];
-        for(int i=0;i<=nums.length;i++){
-            dp[nums.length][i] = 0;
-        }
-        for(int i=n-1;i>=0;i--){
-            for(int j = i-1;j>=-1;j--){
-                int nt = dp[i+1][j+1];
-                int take = 0;
-                if(j==-1 || nums[j]<nums[i]){
-                    take = 1+dp[i+1][i+1];
-                }
-                dp[i][j+1] = Math.max(take,nt);
-            }
-        }
-        return dp[0][0];
+        Integer dp[][] = new Integer[nums.length+1][nums.length+1];
+        return func(0,-1,nums,dp);
     }
 }

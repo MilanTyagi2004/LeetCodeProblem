ac3ab3f02cd5a1ed8e5a4407cfa18c05a1f564bf
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-21 06:30:54 +0530

    Time: 4 ms (78.17%), Space: 46.6 MB (91.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,46 +1,31 @@
 class Solution {
-    int helper(int i,int j,String word1 ,String word2,Integer dp[][]){
-        if(i==word1.length()){
-            return word2.length() - j;
+    public int minDistance(String s, String s1) {
+        int n = s.length();
+        int m =s1.length();
+        int prev[] =new int[m+1];
+        for(int i=0;i<=m;i++){
+            prev[i] = i;
         }
-        if(j==word2.length()){
-            return word1.length()- i;
-        }
-        if(word1.charAt(i) ==  word2.charAt(j)){
-            return helper(i+1,j+1,word1,word2,dp);
-        }
-        if(dp[i][j]!=null){
-            return dp[i][j];
-        }
-        int l = helper(i,j+1,word1,word2,dp);
-        int f = helper(i+1,j,word1,word2,dp);
-        int g = helper(i+1,j+1,word1,word2,dp);
+        for(int i=1;i<=n;i++){
+            int curr[] = new int[m+1];
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==s1.charAt(j-1)){
+                    curr[j] = prev[j-1];
+                }
+                else{
+                    int insert = curr[j - 1];
 
-        return dp[i][j] = 1 + Math.min(l,Math.min(f,g));
-    }
-    public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()+1][word2.length()+1];
-        int m = word1.length();
-        int n = word2.length();
-        for(int i = 0; i <= m; i++){
-            dp[i][0] = i;
-        }
-        for(int j = 0; j <= n; j++){
-            dp[0][j] = j;
-        }
+                    int delete = prev[j];
 
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                int l = dp[i][j-1];
-                int r = dp[i-1][j];
-                int g = dp[i-1][j-1];
-                dp[i][j] =1+ Math.min(l,Math.min(r,g));
+                    int replace = prev[j - 1];
+
+                    curr[j] = 1 + Math.min(insert,
+                                  Math.min(delete, replace));
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[m];
     }
 }

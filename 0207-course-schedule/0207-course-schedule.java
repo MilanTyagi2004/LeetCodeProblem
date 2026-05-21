4c6e316db3e5e34d308aa534560447d8638fb25d
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-21 21:03:46 +0530

    Time: 7 ms (59.37%), Space: 47.3 MB (19.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,38 @@
 class Solution {
-    boolean dfs(int i,boolean vis[],boolean []path,ArrayList<ArrayList<Integer>>all){
-        vis[i] = true;
-        path[i] = true;
-        for(int it:all.get(i)){
-            if(!vis[it]){
-                if(dfs(it,vis,path,all))return true;
-            }
-                else if(path[it]){
-                    return true;
-                }
-        }
-        path[i] =false;
-        return false;
-    }
     public boolean canFinish(int V, int[][] edges) {
-        // main moto is to check if there is any cycle present in this or not .i fpresent then return false if not then return true.
-
-        ArrayList<ArrayList<Integer>> all= new ArrayList<>();
-        for(int i=0;i<=V;i++){
+        // now use toposort to detect cycle in this
+        ArrayList<ArrayList<Integer>>all = new ArrayList<>();
+        for(int i=0;i<V;i++){
             all.add(new ArrayList<>());
-        }
-
+        } 
         for(int edge[]:edges){
-            int u = edge[0];
-            int v = edge[1];
+            int u =edge[0];
+            int v =edge[1];
             all.get(v).add(u);
         }
-        boolean vis[] = new boolean[V];
-        boolean path[] = new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-            if(dfs(i,vis,path,all)){
-            return false;
+        int [] degree = new int[V];
+        for(ArrayList<Integer>al:all){
+            for(int it:al){
+                degree[it]++;
             }
         }
+        Queue<Integer>q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(degree[i]==0){
+                q.add(i);
+            }
         }
-        return true;
+        int cnt=0;
+        while(!q.isEmpty()){
+            cnt++;
+            int node= q.poll();
+            for(int it:all.get(node)){
+                degree[it]--;
+                if(degree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        return cnt==V;
     }
 }

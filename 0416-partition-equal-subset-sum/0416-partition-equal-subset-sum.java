0fde6a0253525d3ef3804a8f95e6d6e185235b12
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-21 06:04:24 +0530

    Time: 52 ms (42.52%), Space: 47 MB (63.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,45 +1,28 @@
 class Solution {
-    boolean memo(int i,int target, int[] nums, Boolean dp [][]){
-        if(target==0){
-            return true;
-        }
-        if(i==nums.length){
-            return false;
-        }
-        if(dp[i][target]!=null){
-            return dp[i][target];
-        }
-        boolean take = false;
-        if(nums[i]<=target){
-        take = memo(i+1,target-nums[i],nums,dp);
-        }
-        boolean nottake = memo(i+1,target,nums,dp);
-        return dp[i][target] = take|| nottake;  
-    }
     public boolean canPartition(int[] nums) {
-        int total = 0;
+        int n = nums.length;
+        int total =0;
         for(int i:nums){
-            total +=i;
+            total+=i;
         }
         if(total%2!=0){
             return false;
         }
-        total  = total/2;
-        boolean dp[][] = new boolean[nums.length+1][total+1];
-        for(int j=0;j<=nums.length;j++){
-            dp[j][0]  = true;
+        int target = total/2;
+        boolean dp [][] = new boolean [n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = true;
         }
-        // tabulation 
-        for(int i=nums.length-1;i>=0;i--){
-            for(int j = 0;j<=total;j++){
-                boolean take = false;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=target;j++){
+                boolean t =false;
                 if(nums[i]<=j){
-                    take = dp[i+1][j-nums[i]];
+                    t = dp[i+1][j-nums[i]];
                 }
-                boolean nottake = dp[i+1][j];
-                dp[i][j] =take || nottake; 
+                boolean nt = dp[i+1][j];
+                dp[i][j] = t||nt;
             }
         }
-        return dp[0][total];
+        return dp[0][target];
     }
 }

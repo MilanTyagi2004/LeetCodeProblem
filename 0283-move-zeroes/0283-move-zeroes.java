e81050ca8a528238f79dfe7ce533fc676e3ad2b2
Milan Tyagi <milantyagi2004@gmail.com>
2026-06-02 12:38:41 +0530

    Time: 2 ms (91.67%), Space: 47.9 MB (18.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,12 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
-
-        int i = 0;
-
-        for (int j = 0; j < arr.length; j++) {
-
-            if (arr[j] != 0) {
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
-                i++;
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            i++;
             }
         }
     }

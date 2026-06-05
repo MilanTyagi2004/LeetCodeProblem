db7a6538dfad1bddf919bebe30be5dfbc7a0e246
Milan Tyagi <milantyagi2004@gmail.com>
2026-06-05 09:43:03 +0530

    Time: 5 ms (82.8%), Space: 77.2 MB (66.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,26 +1,19 @@
 class Solution {
     public int maxArea(int[] nums) {
-        int l = 0;
+        int l =0;
         int r = nums.length-1;
-        int lm = Integer.MIN_VALUE;
-        int rm =Integer.MIN_VALUE;
-        int area = 0;
+        int max =0;
         while(l<r){
-            if(lm<nums[l]){
-                lm = nums[l];
-            }
-            if(rm<nums[r]){
-                rm = nums[r];
-            }
             int len = r-l;
-            int prod = len * Math.min(nums[l],nums[r]);
-            area = Math.max(prod,area);
-           if (nums[l] < nums[r]) {
+            int area = Math.min(nums[l],nums[r]) * len;
+            max = Math.max(max,area);
+            if(nums[l]<nums[r]){
                 l++;
-            } else {
+            }
+            else{
                 r--;
             }
         }
-        return area;
+        return max;
     }
 }

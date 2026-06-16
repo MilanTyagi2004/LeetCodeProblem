3861ed9f7830998f2a47918dacd679756cea0ea6
Milan Tyagi <milantyagi2004@gmail.com>
2026-06-16 15:16:39 +0530

    Time: 0 ms (100%), Space: 42.3 MB (27.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,5 @@
 class Solution {
-    int memo(int i,int j,int m,int n,Integer [][]dp){
+    int helper(int i,int j,int m,int n,Integer [][] dp){
         if(i==m || j==n){
             return 1;
         }
@@ -9,30 +9,27 @@ class Solution {
         if(dp[i][j]!=null){
             return dp[i][j];
         }
-        int right  = memo(i+1,j,m,n,dp);
-        int down = memo(i,j+1,m,n,dp);
-        return dp[i][j] = right+down;
+        int left  = helper(i+1,j,m,n,dp);
+        int right = helper(i,j+1,m,n,dp);
+        return dp[i][j] =left + right;
     }
     public int uniquePaths(int m, int n) {
-        // return recursion(0,0,m-1,n-1);
-
-        Integer dp[][] = new Integer[m+1][n+1];
-        // return memo(0,0,m-1,n-1,dp);
+        int dp[][] = new int[m+1][n+1];
         for(int i=0;i<n;i++){
             dp[0][i] = 1;
         }
+
         for(int j=0;j<m;j++){
-            dp[j][0] =1;
+            dp[j][0]= 1;
         }
 
         for(int i=1;i<m;i++){
-            for(int j =1;j<n;j++){
-                int right = dp[i-1][j];
-                int down = dp[i][j-1];
-                dp[i][j] = right + down;
+            for(int j=1;j<n;j++){
+                int left = dp[i-1][j];
+                int right = dp[i][j-1];
+                dp[i][j] = left +right;
             }
         }
         return dp[m-1][n-1];
-
     }
 }

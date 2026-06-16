21ad69b4c71c51281346773d3ed497b82e4eca7f
Milan Tyagi <milantyagi2004@gmail.com>
2026-06-16 14:54:56 +0530

    Time: 0 ms (100%), Space: 47 MB (10.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,19 @@
 class Solution {
-    void helper(int image[][],int i,int j,int color,int newColor){
-        if(i<0 || j<0 || i>=image.length || j>=image[0].length || image[i][j]!=color || image[i][j] ==newColor){
+    void dfs(int [][] grid,int sr, int sc,int color,int newColor){
+        if(sr<0 || sr>= grid.length || sc<0 || sc>=grid[0].length  || grid[sr][sc]!=newColor ){
             return;
         }
-        image[i][j]=newColor;
-        helper(image,i+1,j,color,newColor);
-        helper(image,i,j+1,color,newColor);
-        helper(image,i-1,j,color,newColor);
-        helper(image,i,j-1,color,newColor);
+        grid[sr][sc] = color;
+        dfs(grid,sr+1,sc,color,newColor);
+        dfs(grid,sr-1,sc,color,newColor);
+        dfs(grid,sr,sc+1,color,newColor);
+        dfs(grid,sr,sc-1,color,newColor);
+
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        helper(image,sr,sc,image[sr][sc],color);
+        if (image[sr][sc] == color)
+        return image;
+        dfs(image, sr, sc, color,image[sr][sc]);
         return image;
     }
 }

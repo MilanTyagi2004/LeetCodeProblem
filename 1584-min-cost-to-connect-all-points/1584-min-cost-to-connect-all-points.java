a4e8c258176e82e435fbfda895d66a8be7fdd0a7
Milan Tyagi <milantyagi2004@gmail.com>
2026-07-02 17:51:52 +0530

    Time: 77 ms (67.25%), Space: 76.9 MB (55.46%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    class Pair {
+        int node;
+        int cost;
+
+        Pair(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+
+        boolean[] vis = new boolean[n];
+
+        PriorityQueue<Pair> pq =
+                new PriorityQueue<>((a, b) -> a.cost - b.cost);
+
+        pq.offer(new Pair(0, 0));
+
+        int ans = 0;
+        int count = 0;
+
+        while (count < n) {
+
+            Pair cur = pq.poll();
+
+            if (vis[cur.node])
+                continue;
+
+            vis[cur.node] = true;
+            ans += cur.cost;
+            count++;
+
+            for (int i = 0; i < n; i++) {
+
+                if (!vis[i]) {
+
+                    int dist = Math.abs(points[cur.node][0] - points[i][0]) +
+                               Math.abs(points[cur.node][1] - points[i][1]);
+
+                    pq.offer(new Pair(i, dist));
+                }
+            }
+        }
+
+        return ans;
+    }
+}

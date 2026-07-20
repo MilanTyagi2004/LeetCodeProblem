8c5bc52a83a42e4436b99f3ea3afc9956095e757
Milan Tyagi <milantyagi2004@gmail.com>
2026-07-20 23:17:42 +0530

    Time: 34 ms (65.92%), Space: 43.4 MB (99.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,19 +3,18 @@ class Solution {
         int n = nums.length;
         int total = 0;
         for(int i:nums){
-            total+=i;
+            total +=i;
         }
         if(total%2!=0){
             return false;
         }
         int target = total/2;
         boolean dp[] = new boolean[target+1];
-        dp[0] = true;
+        dp[0] =true;
+
         for(int i:nums){
             for(int j=target;j>=i;j--){
-                boolean take =dp[j-i];
-                boolean nt = dp[j];
-                dp[j] = take||nt;
+                dp[j] = dp[j] || dp[j-i];
             }
         }
         return dp[target];
